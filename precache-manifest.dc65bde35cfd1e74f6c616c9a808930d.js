63d5e577368371d183b69e7120df43f153f674ad
Ngu Mai <ngumh@imt-soft.com>
2020-01-02 14:11:15 +0700

    Updates

---
+++ precache-manifest.dc65bde35cfd1e74f6c616c9a808930d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "958ccc22e3448517b9172ad68cd77d56",
+    "url": "/cv/index.html"
+  },
+  {
+    "revision": "c6652b44d8703e36f0e7",
+    "url": "/cv/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "971c14b2f71e692c22b7",
+    "url": "/cv/static/js/2.aa64784a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/cv/static/js/2.aa64784a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c6652b44d8703e36f0e7",
+    "url": "/cv/static/js/main.1096ec86.chunk.js"
+  },
+  {
+    "revision": "4157253bc5d6dffe1da9",
+    "url": "/cv/static/js/runtime-main.3c78338b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cv/static/media/logo.5d5d9eef.svg"
+  }
+]);
