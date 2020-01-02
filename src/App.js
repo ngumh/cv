b6e88c65f8149eb2ff8a585455e6dae1e1f7d3e4
Ngu Mai <ngumh@imt-soft.com>
2020-01-02 22:25:25 +0700

    Update Responsive

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import 'bulma/css/bulma.css'
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Header />
       <Content />
       <Footer />
--- src/components/elements/Hero.js
+++ src/components/elements/Hero.js
@@ -15,25 +15,6 @@ function Hero() {
                     <h2 className="subtitle has-text-white">{Resume.basics.location.region}, {Resume.basics.location.country}</h2>
                 </div>
             </div>
-            <div className="hero-foot" style={{paddingBottom:'20px'}}>
-                <div className="columns is-mobile">
-                    <div className="column is-4"></div>
-                    {
-                        Resume.basics.profiles.map((value, index) => {
-                            return (
-                                <div key={index} className="column is-2 has-text-centered">
-                                    <a href={value.url} target="blank" className="is-hovered" title={value.network}>
-                                        <span className="icon is-medium has-text-white">
-                                            <i className={value.x_icon}></i>
-                                        </span>
-                                    </a>
-                                </div>
-                            );
-                        })
-                    }
-                    <div className="column is-4"></div>
-                </div>
-            </div>
         </section>
     )
 }
