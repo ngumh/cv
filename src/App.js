dc28174f305fecbbb7aa26f28a78980a0c71ba79
Ngu Mai <ngumh@imt-soft.com>
2020-01-06 13:33:47 +0700

    Add Emotion + Loading

--- src/App.js
+++ src/App.js
@@ -2,23 +2,33 @@ import React from 'react';
 import Header from "./components/structure/Header"
 import Content from "./components/structure/Content"
 import Footer from "./components/structure/Footer"
-
+import { css } from "@emotion/core";
+import { HashLoader } from "react-spinners";
 import './App.css';
 import 'bulma/css/bulma.css'
 
 function App() {
   const [isLoading, setisLoading] = React.useState(true);
-
+  const override = css`
+    margin: 0;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  `;
   React.useEffect(() => {
-    setTimeout(() => setisLoading(false), 1000);
+    setTimeout(() => setisLoading(false), 1500);
   });
 
   return (
-    <div>
-      {isLoading 
-      ?
-      <progress class="progress is-small is-dark" max="100">15%</progress>
-      : 
+    <div >
+      {isLoading ? <HashLoader    
+          css={override}
+          size={"100px"}
+          color={"#e4d00a"}
+          loading={setisLoading}
+        />
+      :
         <div>
           <Header />
           <Content />
--- src/components/structure/Footer.js
+++ src/components/structure/Footer.js
@@ -4,7 +4,12 @@ function Footer() {
     return (
         <footer className="footer has-background-dark">
             <div className="content has-text-centered has-text-white">
-                <i><sup><i className="fas fa-quote-left"></i></sup> Success is not an accident, success is actually a choice. <sup><i className="fas fa-quote-right"></i></sup></i>
+                <i>
+                    <sup><i className="fas fa-quote-left"></i></sup>
+                        Success is not an accident, success is actually a choice. 
+                    <sup><i className="fas fa-quote-right"></i></sup>
+                    &nbsp; <sub>- &nbsp;Stephen Curry</sub>
+                </i>
                 <p>
                     <img width="512" height="96" src="https://bulma.io/images/made-with-bulma--semiwhite.png" className="bulma-image" alt="" />
                 </p>
