43c2b693fa196035458b84f4534b6440b7b0c32a
Ngu Mai <ngumh@imt-soft.com>
2020-01-11 16:44:54 +0700

    Updates

---
+++ precache-manifest.6935c525ab993fd092ad7fb19aab2331.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03f3d7a9ba0273ea4e8064f30e39a548",
+    "url": "/cv/index.html"
+  },
+  {
+    "revision": "970a3e50a2e7c7207abd",
+    "url": "/cv/static/css/2.c4418c31.chunk.css"
+  },
+  {
+    "revision": "cc6d28a97ca96018604a",
+    "url": "/cv/static/css/main.96b7af32.chunk.css"
+  },
+  {
+    "revision": "970a3e50a2e7c7207abd",
+    "url": "/cv/static/js/2.91019234.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/cv/static/js/2.91019234.chunk.js.LICENSE"
+  },
+  {
+    "revision": "98a0272f6adee3ce7d73",
+    "url": "/cv/static/js/3.3456d907.chunk.js"
+  },
+  {
+    "revision": "2667ef628ff0ba729a4d",
+    "url": "/cv/static/js/4.aaafe155.chunk.js"
+  },
+  {
+    "revision": "7283725dd1931bf2d29d",
+    "url": "/cv/static/js/5.97fde72c.chunk.js"
+  },
+  {
+    "revision": "cc6d28a97ca96018604a",
+    "url": "/cv/static/js/main.97e7d7e7.chunk.js"
+  },
+  {
+    "revision": "cf3cf1a350e771081e69",
+    "url": "/cv/static/js/runtime-main.d12b8886.js"
+  },
+  {
+    "revision": "5c1229f5f260968cc7603aa4ce77df10",
+    "url": "/cv/static/media/bg.5c1229f5.JPG"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cv/precache-manifest.8b029499037aa131a499868c9ed5fab0.js"
+  "/cv/precache-manifest.6935c525ab993fd092ad7fb19aab2331.js"
 );
 
 self.addEventListener('message', (event) => {
